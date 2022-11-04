9011adad0e8b962f74c78c2cbb3f474530adc0a8
Pablo <ptorres_65@hotmail.com>
2022-11-04 13:40:43 +0100

    Create shiny_app.R

---
+++ R/shiny_app.R
@@ -0,0 +1,70 @@
+if(!require("shiny")){
+    install.packages("shiny")
+    library(shiny)
+}
+
+if(!require("readxl")){
+    install.packages("readxl")
+    library(readxl)
+}
+
+# Uploading and parsing the file.
+
+ui_upload = sidebarLayout(
+  sidebarPanel(
+    fileInput("file", "data", buttonLabel = "Upload file")
+  ), # ending of sidebarPanel
+  mainPanel(
+    h3("TALIS 2024 - Within school sampling selection - Beta"),
+    tableOutput("preview1")
+  )
+) # ending of sidebarLayout
+
+
+# Download the file.
+
+ui_download = fluidRow(
+  column(width = 12, downloadButton("download", class = "btn-block"))
+) # ending of fluidRow
+
+# assembled into a single fluidPage();
+
+ui = fluidPage(ui_upload,ui_download)
+
+# ----------------------------------------------
+
+server = function(input,output,session){
+  # Upload -------------------------------------
+  data = reactive({
+    req(input$file)
+    
+    ext = tools::file_ext(input$file$name)
+    switch(ext,
+           xlsx = readxl::read_xlsx(input$file$datapath),
+           validate("Invalid file; Please upload a .xlsx file"))
+  })
+  output$preview1 = renderTable(head(data()))
+  
+  # Clean step --------------------------------
+  
+  transform = reactive({
+    db = data()
+    
+    if(1 == 1){
+      df = db[8:15,]
+    }
+    df
+  })
+  
+  output$preview2 = renderTable(transform())
+  
+  # Download -----------------------------------
+  
+  output$download = downloadHandler(
+    filename = function(){paste0(tools::file_path_sans_ext(input$file$name), ".xlsx")
+    },
+    content = function(file){
+      writexl::write_xlsx(transform(), path = file)
+    }
+  )
+}
