7a302f159d615b8e50e9bfc6d724d278021fa1f1
Pablo <ptorres_65@hotmail.com>
2022-11-28 15:48:24 +0100

    Create fill_df.R
    Function to remove and replace invalid values to 0.
    
    With this function we are allowed to identify and check inconsistencies within the uploaded data frame.

---
+++ R/fill_df.R
@@ -0,0 +1,17 @@
+fill_df = function(x){
+# now I have only 0 values.
+clean_df = as.data.frame(apply(x,2,FUN=function(x){ifelse(is.na(x)==TRUE,0,x)}))
+
+clean_df[,4]  = ifelse(nchar(clean_df[,4]) == 4,clean_df[,4],0)
+clean_df[,5]  = ifelse(clean_df[,5] %in% c(1,2,3,9),clean_df[,5],0)
+clean_df[,6]  = ifelse(clean_df[,6] == 1,clean_df[,6],0)
+clean_df[,7]  = ifelse(clean_df[,7] == 1,clean_df[,7],0)
+clean_df[,8]  = ifelse(clean_df[,8] == 1,clean_df[,8],0)
+# if there is 02 values at least one of them has to be leader
+clean_df[,9]  = ifelse(clean_df[,9] == 1,clean_df[,9],0)
+clean_df[,10] = ifelse(clean_df[,10] %in% c(1,2,3,4,5,6,7,8,9,11,12),clean_df[,10],0)
+clean_df[,11] = ifelse(clean_df[,11] %in% c(1:4,9),clean_df[,11],0)
+clean_df[,12] = ifelse(clean_df[,12] %in% c(1:4,9),clean_df[,12],0)
+clean_df
+}
+
