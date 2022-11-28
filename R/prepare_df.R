86a6d7892bce0f61dc6a582402717c07e4677f1d
Pablo <ptorres_65@hotmail.com>
2022-11-28 15:46:41 +0100

    Create prepare_df.R
    Function to read and start the cleaning process of the data frame

---
+++ R/prepare_df.R
@@ -0,0 +1,11 @@
+prepare_df = function(x){
+	x = as.data.frame(x)
+	db_pi = which(x[,1] == '<_fld_teacher_name_>' | x[,12] == "<_list_end_>" | x[,5] == "X" )
+	db_co = nrow(x)
+	db_er = which(rowSums(is.na(x)) == ncol(x))
+	row_rm = c(db_pi,db_co,db_er)
+	df = x[!1:dim(x)[1] %in% row_rm,]
+	df
+}
+
+
