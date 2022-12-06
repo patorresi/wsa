0d8b51b529912c2e994d8b4e11a9852b7b571b8a
Pablo <18447158+patorresi@users.noreply.github.com>
2022-12-06 16:07:14 +0100

    Update check_df.R
    Fixed warning 1 and add comments to each one of the mentioned warnings.

--- R/check_df.R
+++ R/check_df.R
@@ -19,52 +19,58 @@ i10v = sum(as.numeric(df[,7])) > 0
 # ISCED level 2 values 
 i20v = sum(as.numeric(df[,8])) > 0
 
+# Rule 0
+# All the inscribed members need to have at least their name, birthday and gender.
+w0 = length(which(df[,1] == 0 & df[,4] == 0 & df[,5] == 0)) == 0 
 
-w0 = length(which(df[,1] == 0, df[,4] == 0 | df[,5] == 0)) == 0 
-
-
-# only one leader
+# Rule 1
+# Check if there is only one lider in ISCED level 02.
 w1 = if(i02v == TRUE){
   sum(as.numeric(df[,9])) == 1
 }else{
   sum(as.numeric(df[,9])) == 0
 }
 
-# staff role 
-w2 = if(i02v == TRUE){
-  sum(df[,10] %in% c(1,2,3,4,5,6,7,8,9,11,12)) == nrow(df)
-}else{
-  sum(df[,10] %in% c(1,2,3,4,5,6,7,8,9,11,12)) == 0
-}
+# Function to check valid values.
+# if x is a valid teacher (1) their domain need to be in a specific range of valid
+# responses.
 
-
-fun1 = function(x,y){
+i02_valid_r = c(1,2,3,4,5,6,7,8,9,10,11,12)
+fun_i02 = function(x,y){
   if(x == 1){
-     y != 0
+     y %in% i02_valid_r
   }else{
      y == 0
   }
 }
 
-# domain isced level 1
+# Rule 2
+# Check if the staff as valid Staff Role
+w2 = sum(mapply(fun_i02,df[,6],df[,10])) == nrow(df)
+  
+fun1 = function(x,y){
+  if(x == 1){
+     y %in% c(1,2,3,4,9)
+  }else{
+     y == 0
+  }
+}
+
+# Rule 3
+# Check if the teacher as valid Main Subject domain
 w3 = sum(mapply(fun1,df[,7],df[,11])) == nrow(df)
 
-#if(i02v == TRUE){
-#  sum(df[,11] %in% c(1:4,9)) == nrow(df)
-#}else{
-#  sum(df[,11] %in% c(1:4,9)) == 0
-#}
-
-# domain isced level 2
+# Rule 4
+# Check if the teacher as valid Main Subject domain
 w4 = sum(mapply(fun1,df[,8],df[,12])) == nrow(df)
 
-# all teacher listed need to have at least one participation in one isced level
-w5 = sum(apply(df[,c(6,7,8)],1,FUN=function(x){sum(as.numeric(x))}) >= 1) == nrow(df)
-
-w6 = (i02v + i10v + i20v)<=2
-
-
-value = sum(c(w0,w1,w2,w3,w4,w5,w6))==length(c(w0,w1,w2,w3,w4,w5,w6))
+# Rule 5
+# All teachers or staff needed be list it at least one ISCED level
+# and no more than 2. 
+w5 = sum(apply(df[,c(6,7,8)],1,FUN=function(x){sum(as.numeric(x))}) >= 1 &
+apply(df[,c(6,7,8)],1,FUN=function(x){sum(as.numeric(x))}) <= 2) == nrow(df)
+ 
+value = sum(c(w0,w1,w2,w3,w4,w5))== length(c(w0,w1,w2,w3,w4,w5))
 
 # print("You should revise the list form. Some of the rows may not contain all the necesary fields completed.")
 # print("Ready to go!")
